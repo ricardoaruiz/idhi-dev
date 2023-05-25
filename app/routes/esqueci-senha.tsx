f1b6754d19dc286c9734737a0ca871cb099e1d8f
Ricardo Ruiz <ricardo.ruiz@grupoboticario.com.br>
2023-05-25 14:29:04 -0300

    adding a debug log

--- app/routes/esqueci-senha.tsx
+++ app/routes/esqueci-senha.tsx
@@ -47,8 +47,10 @@ const sendRequestPasswordMutation = makeDomainFunction(schema)(async (data) => {
  */
 const sendChangePasswordEmail = async (name: string, email: string) => {
   try {
+    console.log('esqueci-senha.tsx-#1 - Vai mandar o email')
     await sendEmail({ name, email, template: EMAIL_TEMPLATE.FORGOT_PASSWORD })
-    
+    console.log('esqueci-senha.tsx-#2 - Mandou o email')
+
     return json({
       error: '',
       success: 'E-mail com link para troca de senha enviada com sucesso',
@@ -57,7 +59,7 @@ const sendChangePasswordEmail = async (name: string, email: string) => {
       }
     })
   } catch(error) {
-    console.error('esqueci-senha.tsx-#1', error)
+    console.log('esqueci-senha.tsx-#3 - Erro ao enviar o email', error)
     return json({
       error: 'Erro no envio do e-mail de solicitação de senha',
       success: '',
